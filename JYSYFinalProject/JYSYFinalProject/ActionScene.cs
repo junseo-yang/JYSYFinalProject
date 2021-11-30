2e0dc1ab888e6fb283983c062c687b387bf3b02e
seongjoyun <95135711+seongjoyun@users.noreply.github.com>
2021-11-29 19:11:27 -0500

    Update ActionScene

--- JYSYFinalProject/JYSYFinalProject/ActionScene.cs
+++ JYSYFinalProject/JYSYFinalProject/ActionScene.cs
@@ -10,10 +10,72 @@ namespace JYSYFinalProject
     public class ActionScene : GameScene
     {
         private SpriteBatch spriteBatch;
+        private Bow bow;
+        private int fireballCooltime = 0;
+        private int tornadoCooltime = 0;
+        private MouseState oldMouseState;
+        private KeyboardState oldKeyboardState;
         public ActionScene(Game game) : base(game)
         {
             Game1 g = (Game1)game;
             this.spriteBatch = g._spriteBatch;
+            Texture2D texCastle = g.Content.Load<Texture2D>("images/Castle");
+            Castle castle = new Castle(game, spriteBatch, texCastle);
+
+            Texture2D texBow = g.Content.Load<Texture2D>("images/Bow");
+            bow = new Bow(game, spriteBatch, texBow);
+
+            this.SceneComponents.Add(castle);
+            this.SceneComponents.Add(bow);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MouseState ms = Mouse.GetState();
+            float xDiff = ms.X - bow.position.X;
+            float yDiff = ms.Y - bow.position.Y;
+            if (xDiff < 0)
+            {
+                if (ms.LeftButton == ButtonState.Pressed && oldMouseState.LeftButton == ButtonState.Released && bow.frameIndexCol==0)
+                {
+                    Vector2 speed = new Vector2(xDiff * 0.02f, yDiff * 0.02f);
+                    Arrow arrow = new Arrow(this.Game, spriteBatch, this.Game.Content.Load<Texture2D>("images/Arrow"), bow.position, speed, bow.rotation);
+                    this.SceneComponents.Add(arrow);
+                }
+                if (ms.RightButton == ButtonState.Pressed && tornadoCooltime == 0)
+                {
+                    Vector2 speed = new Vector2(xDiff * 0.025f, yDiff * 0.025f);
+                    Tornado tornado = new Tornado(this.Game, spriteBatch, this.Game.Content.Load<Texture2D>("images/Tornado"), bow.position, speed);
+                    this.SceneComponents.Add(tornado);
+                    tornadoCooltime = 300;
+                }
+            }
+
+
+            KeyboardState ks = Keyboard.GetState();
+
+            if(ks.IsKeyDown(Keys.Left) && oldKeyboardState.IsKeyUp(Keys.Left) && fireballCooltime == 0)
+            {
+                Fireball fireball = new Fireball(this.Game, spriteBatch, this.Game.Content.Load<Texture2D>("images/Fireball"), bow.position);
+                this.SceneComponents.Add(fireball);
+                fireballCooltime = 300;
+            }
+            if(fireballCooltime>0)
+            {
+                fireballCooltime--;
+            }
+            if(tornadoCooltime > 0)
+                {
+                tornadoCooltime--;
+            }
+
+
+
+            oldMouseState = ms;
+            oldKeyboardState = ks;
+
+
+            base.Update(gameTime);
         }
     }
 }
