0f7ee933b03b5c21f8527c17d7ab4a7aa1af5380
seongjoyun <95135711+seongjoyun@users.noreply.github.com>
2021-11-29 19:10:59 -0500

    Create Castle

---
+++ JYSYFinalProject/JYSYFinalProject/Castle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JYSYFinalProject
+{
+    public class Castle : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private Texture2D tex;
+        private Vector2 position;
+
+        public Castle(Game game,
+            SpriteBatch spriteBatch,
+            Texture2D tex) : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+            this.tex = tex;
+            this.position = new Vector2((Shared.stage.X-tex.Width),0);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(tex, position, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
